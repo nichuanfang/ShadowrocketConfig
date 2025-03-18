60f562fb4957754bd2d60ce56e9c94deeb1a35e9
nichuanfang <f18326186224@gmail.com>
2025-03-18 14:32:45 +0800

    chore: update shadowrocket

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 # 处理小火箭配置文件的入口文件
 
 import os
+import re
 from datetime import datetime
-
-import os
-from datetime import datetime
+import requests
 
 # 通用模块
 general_module_line = '[General]'
 #路由模块
 rule_module_line = '[Rule]'
+# 直连地址
+direct_url = 'https://cdn.jsdelivr.net/gh/GMOogway/shadowrocket-rules@master/sr_direct_list.module'
+# 代理地址
+proxy_url = 'https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_proxy_list.module'
+# 拦截地址
+reject_url = 'https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_reject_list.module'
+
 
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -19,32 +25,50 @@ def write_file(file_path, content):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w+', encoding='utf-8') as file:
         file.write(content)
+        
+def extract_rules_from(source: str) -> str or None:
+    """
+    从源文本中提取 [Rule] 部分的内容。
+
+    Args:
+        source (str): 包含规则的源文本。
+
+    Returns:
+        str or None: 如果找到 [Rule] 部分，则返回提取的规则字符串；否则返回 None。
+    """
+    raw = requests.get(direct_url)
+    text = raw.text
+    # 使用正则表达式查找 [Rule] 部分及其后面的所有内容，直到文本结束
+    start_index = text.find("[Rule]")
+    if start_index != -1:
+        rules = text[start_index + len("[Rule]"):]
+        return rules.strip()
+    else:
+        return None
 
 def replace_direct(strategy_content):
     """
     替换策略内容中的 {direct} 占位符为 direct.conf 文件中的实际内容。
     """
-    # direct_content = read_file('config/rules/direct.conf')
-    # return strategy_content.replace('{direct}', direct_content)
-    pass
+    result = extract_rules_from(direct_url)
+    if result:
+        return strategy_content.replace('{direct}', result)
 
 def replace_reject(strategy_content):
     """
     替换策略内容中的 {reject} 占位符为 reject.conf 文件中的实际内容。
     """
-    # reject_content = read_file('config/rules/reject.conf')
-    # return strategy_content.replace('{reject}', reject_content)
-    pass
+    result = extract_rules_from(direct_url)
+    if result:
+        return strategy_content.replace('{reject}', result)
 
 def replace_proxy(strategy_content):
     """
     替换策略内容中的 {proxy} 占位符为通过 HTTP 请求获取的远程 URL 内容。
     """
-    # import requests
-    # response = requests.get('http://example.com/proxy.conf') # 替换为实际的URL
-    # proxy_content = response.text
-    # return strategy_content.replace('{proxy}', proxy_content)
-    pass
+    result = extract_rules_from(direct_url)
+    if result:
+        return strategy_content.replace('{proxy}', result)
 
 def replace_direct_accelerate(strategy_content):
     """
@@ -73,9 +97,9 @@ def process_strategy(general_content, base_content, strategy_content, custom_dir
     strategy_content = strategy_content.replace('{custom-direct}', custom_direct) + '\n'
     strategy_content = strategy_content.replace('{custom-proxy}', custom_proxy) + '\n'
     strategy_content = strategy_content.replace('{custom-reject}', custom_reject) + '\n'
-    # strategy_content = replace_direct(strategy_content) + '\n'
-    # strategy_content = replace_reject(strategy_content) + '\n'
-    # strategy_content = replace_proxy(strategy_content) + '\n'
+    strategy_content = replace_direct(strategy_content) + '\n'
+    strategy_content = replace_reject(strategy_content) + '\n'
+    strategy_content = replace_proxy(strategy_content) + '\n'
     # strategy_content = replace_direct_accelerate(strategy_content) + '\n'
     # strategy_content = replace_proxy_accelerate(strategy_content) + '\n'
     
